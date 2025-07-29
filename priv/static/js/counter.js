09554bbaf66e7f5fa449f206d84dddf9499d0cb3
yosu <woodstock830@gmail.com>
2025-07-29 19:19:47 +0900

    Added background color

--- priv/static/js/counter.js
+++ priv/static/js/counter.js
@@ -1,3 +1,13 @@
+function randomPair(upperBound) {
+  const a = Math.floor(Math.random() * upperBound)
+  let b = a;
+  while (a === b) {
+    b = Math.floor(Math.random() * upperBound)
+  }
+
+  return [a, b];
+}
+
 function main() {
   svg = d3.select("svg")
     .style("caret-color", "transparent") // hide the caret cursor
@@ -11,18 +21,40 @@ function main() {
   const g2 = svg.append("g")
     .attr("transform", `translate(0, ${height/2})`)
 
+  const drawBackground = (g, width, height, fill) => {
+    g.append("rect")
+      .attr("class", "bg")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", width)
+      .attr("height", height)
+      .attr("fill", fill)
+  }
+
+  const updateBackground = (g, fill) => {
+    g.select(".bg")
+      .transition()
+      .attr("fill", fill)
+  }
+
+  const colors = [
+    d3.schemePaired[0], d3.schemePaired[1],
+    d3.schemePaired[2], d3.schemePaired[3],
+    d3.schemePaired[6], d3.schemePaired[7],
+    d3.schemePaired[8], d3.schemePaired[9],
+    "lightgray", "gray",
+    "firebrick", "mistyrose",
+  ]
+  const pair = randomPair(colors.length)
+
+  drawBackground(g1, width, height/2, colors[pair[0]])
+  drawBackground(g2, width, height/2, colors[pair[1]])
+
   const reset1 = createViz(g1, width, height/2)
   const reset2 = createViz(g2, width, height/2)
 
-  svg.append("line")
-    .attr("x1", 10)
-    .attr("y1", height/2)
-    .attr("x2", width-20)
-    .attr("y2", height/2)
-    .attr("stroke", "gray")
-
-  const resetWidth = 120;
-  const resetHeight = 60;
+  const resetWidth = 100;
+  const resetHeight = 100;
   const resetX = width / 2 - resetWidth / 2;
   const resetY = height / 2 - resetHeight / 2;
 
@@ -31,10 +63,15 @@ function main() {
     .attr("y", resetY)
     .attr("width", resetWidth)
     .attr("height", resetHeight)
-    .attr("fill", "white")
+    .attr("fill", "transparent")
     .on("click", () => {
       reset1()
       reset2()
+
+      const cols = randomPair(colors.length)
+
+      updateBackground(g1, colors[cols[0]])
+      updateBackground(g2, colors[cols[1]])
     })
 
     /*
@@ -53,6 +90,14 @@ function main() {
 
   const drawResetIcon = (g) => {
     const r = 24;
+    const fill = "black"
+
+    g.append("circle")
+      .attr("cx", 0)
+      .attr("cy", 0)
+      .attr("r", r + 16)
+      .attr("fill", "lightgray")
+      .attr("pointer-events", "none")
 
     g.append("path")
       .attr("d", d3.arc()({
@@ -61,7 +106,7 @@ function main() {
         startAngle: -Math.PI * 0.4,
         endAngle: Math.PI * 0.4
       }))
-      .attr("fill", "gray")
+      .attr("fill", fill)
       .attr("pointer-events", "none")
 
     g.append("path")
@@ -71,18 +116,18 @@ function main() {
         startAngle: Math.PI * 1.4,
         endAngle: Math.PI * 0.6
       }))
-      .attr("fill", "gray")
+      .attr("fill", fill)
       .attr("pointer-events", "none")
 
     g.append("path")
       .attr("transform", `translate(${r*Math.cos(Math.PI)}, ${r * Math.sin(Math.PI*0.9)}) rotate(${-20}) scale(1.2)`)
       .attr("d", d3.symbol(d3.symbolTriangle))
-      .attr("fill", "gray")
+      .attr("fill", fill)
 
     g.append("path")
       .attr("transform", `translate(${r*Math.cos(-Math.PI*0)}, ${r * Math.sin(-Math.PI*0.1)}) rotate(${35}) scale(1.2)`)
       .attr("d", d3.symbol(d3.symbolTriangle))
-      .attr("fill", "gray")
+      .attr("fill", fill)
 
   }
 
@@ -93,15 +138,16 @@ function createViz(g, width, height) {
   let data = [{value: 20}]
 
   const createButtons = () => {
-    const buttons = ["+", "-"]
+    const buttons = ["-", "+"]
+    const stroke = "white"
 
-    const r = 38;
+    const r = 45;
 
     g.selectAll(".button-circle")
       .data(buttons)
       .join("circle")
       .attr("class", ".button-circle")
-      .attr("cx", (_d, i) => width/4+width/2*i)
+      .attr("cx", (_d, i) => width/6+width/1.5*i)
       .attr("cy", height/2)
       .attr("r", r)
       .attr("fill", "transparent")
@@ -126,31 +172,32 @@ function createViz(g, width, height) {
 
     g.append("line")
       .attr("class", "minus")
-      .attr("x1", width * 0.75 - 12)
+      .attr("x1", width * 0.2 - 12)
       .attr("y1", height/2)
-      .attr("x2", width * 0.75 + 12)
+      .attr("x2", width * 0.2 + 12)
       .attr("y2", height/2)
-      .attr("stroke", "gray")
+      .attr("stroke", stroke)
       .attr("stroke-width", 4)
       .attr("stroke-linecap", "round")
 
     g.append("line")
       .attr("class", "plus")
-      .attr("x1", width * 0.25 - 12)
+      .attr("x1", width * 0.8 - 12)
       .attr("y1", height/2)
-      .attr("x2", width * 0.25 + 12)
+      .attr("x2", width * 0.8 + 12)
       .attr("y2", height/2)
-      .attr("stroke", "gray")
+      .attr("stroke", stroke)
       .attr("stroke-width", 4)
       .attr("stroke-linecap", "round")
 
+
     g.append("line")
       .attr("class", "plus")
-      .attr("x1", width * 0.25)
+      .attr("x1", width * 0.8)
       .attr("y1", height/2 - 12)
-      .attr("x2", width * 0.25)
+      .attr("x2", width * 0.8)
       .attr("y2", height/2 + 12)
-      .attr("stroke", "gray")
+      .attr("stroke", stroke)
       .attr("stroke-width", 4)
       .attr("stroke-linecap", "round")
   }
@@ -162,8 +209,9 @@ function createViz(g, width, height) {
       .text(d => d.value)
       .attr("x", width/2)
       .attr("y", height/2)
-      .attr("font-family", "serif")
-      .attr("font-size", 48)
+      .attr("font-family", "Courier, Times New Roman, serif")
+      .attr("font-size", 96)
+      .attr("font-weight", "bold")
       .attr("dominant-baseline", "middle")
       .attr("text-anchor", "middle")
 
