1aba9b54f4f943a7bd808e6ff502acd735e21690
yosu <woodstock830@gmail.com>
2025-07-29 13:40:26 +0900

    Route home redirect to counter page

--- lib/mtg_web/controllers/page_controller.ex
+++ lib/mtg_web/controllers/page_controller.ex
@@ -2,9 +2,7 @@ defmodule MtgWeb.PageController do
   use MtgWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    redirect(conn, to: ~p"/counter")
   end
 
   def counter(conn, _params) do
