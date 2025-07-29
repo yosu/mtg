f972015b68e475c9c67bdf97966f87501b088d55
yosu <woodstock830@gmail.com>
2025-07-29 18:06:49 +0900

    Added reset icon

--- priv/static/js/counter.js
+++ priv/static/js/counter.js
@@ -15,13 +15,13 @@ function main() {
   const reset2 = createViz(g2, width, height/2)
 
   svg.append("line")
-    .attr("x1", 0)
+    .attr("x1", 10)
     .attr("y1", height/2)
-    .attr("x2", width)
+    .attr("x2", width-20)
     .attr("y2", height/2)
     .attr("stroke", "gray")
 
-  const resetWidth = 160;
+  const resetWidth = 120;
   const resetHeight = 60;
   const resetX = width / 2 - resetWidth / 2;
   const resetY = height / 2 - resetHeight / 2;
@@ -31,15 +31,13 @@ function main() {
     .attr("y", resetY)
     .attr("width", resetWidth)
     .attr("height", resetHeight)
-    .attr("rx", 30)
-    .attr("ry", 30)
     .attr("fill", "white")
-    .attr("stroke", "gray")
     .on("click", () => {
       reset1()
       reset2()
     })
 
+    /*
   svg.append("text")
     .text("Reset")
     .attr("x", resetX + resetWidth/2)
@@ -48,6 +46,47 @@ function main() {
     .attr("dominant-baseline", "middle")
     .attr("font-size", 32)
     .attr("pointer-events", "none") // skip click events
+    */
+
+  const g = svg.append("g")
+    .attr("transform", `translate(${width/2}, ${height/2})`)
+
+  const drawResetIcon = (g) => {
+    const r = 24;
+
+    g.append("path")
+      .attr("d", d3.arc()({
+        innerRadius: r,
+        outerRadius: r + 4,
+        startAngle: -Math.PI * 0.4,
+        endAngle: Math.PI * 0.4
+      }))
+      .attr("fill", "gray")
+      .attr("pointer-events", "none")
+
+    g.append("path")
+      .attr("d", d3.arc()({
+        innerRadius: r,
+        outerRadius: r + 4,
+        startAngle: Math.PI * 1.4,
+        endAngle: Math.PI * 0.6
+      }))
+      .attr("fill", "gray")
+      .attr("pointer-events", "none")
+
+    g.append("path")
+      .attr("transform", `translate(${r*Math.cos(Math.PI)}, ${r * Math.sin(Math.PI*0.9)}) rotate(${-20}) scale(1.2)`)
+      .attr("d", d3.symbol(d3.symbolTriangle))
+      .attr("fill", "gray")
+
+    g.append("path")
+      .attr("transform", `translate(${r*Math.cos(-Math.PI*0)}, ${r * Math.sin(-Math.PI*0.1)}) rotate(${35}) scale(1.2)`)
+      .attr("d", d3.symbol(d3.symbolTriangle))
+      .attr("fill", "gray")
+
+  }
+
+  drawResetIcon(g)
 }
 
 function createViz(g, width, height) {
@@ -56,7 +95,7 @@ function createViz(g, width, height) {
   const createButtons = () => {
     const buttons = ["+", "-"]
 
-    const r = 24;
+    const r = 38;
 
     g.selectAll(".button-circle")
       .data(buttons)
@@ -65,18 +104,13 @@ function createViz(g, width, height) {
       .attr("cx", (_d, i) => width/4+width/2*i)
       .attr("cy", height/2)
       .attr("r", r)
-      .attr("stroke", "black")
-      .attr("stroke-width", 2)
-      .attr("fill", "lightgray")
+      .attr("fill", "transparent")
       .on("click", (_e, d) => {
-        if (d === "+") {
-          data[0].value += 1
-        } else {
-          data[0].value -= 1
-        }
+        data[0].value += (d === "+") ? 1 : -1;
         updateCounter()
       })
 
+    /*
     g.selectAll(".button")
       .data(buttons)
       .join("text")
@@ -88,6 +122,37 @@ function createViz(g, width, height) {
       .attr("text-anchor", "middle")
       .attr("dominant-baseline", "middle")
       .attr("pointer-events", "none") // skip click events
+      */
+
+    g.append("line")
+      .attr("class", "minus")
+      .attr("x1", width * 0.75 - 12)
+      .attr("y1", height/2)
+      .attr("x2", width * 0.75 + 12)
+      .attr("y2", height/2)
+      .attr("stroke", "gray")
+      .attr("stroke-width", 4)
+      .attr("stroke-linecap", "round")
+
+    g.append("line")
+      .attr("class", "plus")
+      .attr("x1", width * 0.25 - 12)
+      .attr("y1", height/2)
+      .attr("x2", width * 0.25 + 12)
+      .attr("y2", height/2)
+      .attr("stroke", "gray")
+      .attr("stroke-width", 4)
+      .attr("stroke-linecap", "round")
+
+    g.append("line")
+      .attr("class", "plus")
+      .attr("x1", width * 0.25)
+      .attr("y1", height/2 - 12)
+      .attr("x2", width * 0.25)
+      .attr("y2", height/2 + 12)
+      .attr("stroke", "gray")
+      .attr("stroke-width", 4)
+      .attr("stroke-linecap", "round")
   }
 
   g.selectAll(".counter")
@@ -101,10 +166,6 @@ function createViz(g, width, height) {
       .attr("font-size", 48)
       .attr("dominant-baseline", "middle")
       .attr("text-anchor", "middle")
-    .on("click", (e, d) => {
-        console.log(e)
-        console.log(d)
-      })
 
   const updateCounter = () => {
     g.selectAll(".counter")
